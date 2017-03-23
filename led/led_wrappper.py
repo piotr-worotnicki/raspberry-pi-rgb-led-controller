80664c3dc5c3e8a772559ff511566037d9311d3e
Piotrek <worotnicki@gmail.com>
2017-03-23 23:37:32 +0100

    basic template

--- led/led_wrappper.py
+++ led/led_wrappper.py
@@ -7,6 +7,6 @@ call_template = u'echo "{pin}={value}" > /dev/pi-blaster'
 
 def set_color(r, g, b):
     resolution = float(255)
-    print call_template.format(**{'pin': settings.RED_PIN, 'value': r / resolution})
-    print call_template.format(**{'pin': settings.GREEN_PIN, 'value': g / resolution})
-    print call_template.format(**{'pin': settings.BLUE_PIN, 'value': b / resolution})
+    subprocess.Popen(call_template.format(**{'pin': settings.RED_PIN, 'value': r / resolution}))
+    subprocess.Popen(call_template.format(**{'pin': settings.GREEN_PIN, 'value': g / resolution}))
+    subprocess.Popen(call_template.format(**{'pin': settings.BLUE_PIN, 'value': b / resolution}))
